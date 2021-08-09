cb2a95b6bc0c96ae38d5bb03aa41ec7b8d334294
Hiroyuki-01 <hiroyuki02228n@gmail.com>
2021-08-10 04:26:38 +0900

    .

--- api/server.js
+++ api/server.js
@@ -4,6 +4,7 @@ const line = require('@line/bot-sdk');
 const bot = require('../bot');
 // configの読み込み
 const config = require('../config').index();
+const trigger = require('../trigger').triggerFunc();
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -12,6 +13,10 @@ const app = express();
 app.get('/', (req, res) => { res.send('Deploy succeeded'); });
 // /webhookにアクセスがあったとき、bot.jsのindexを呼び出す
 app.post('/webhook', line.middleware(config), bot.index);
+app.post('/trigger', (req, res) => {
+    trigger();
+    res.end();
+})
 
 app.listen(PORT);
 console.log(`Server running at ${PORT}`);
