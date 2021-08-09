004767656ecd9539aa7cdc403d7fe94c1f562b18
s1290154 <s1290154@u-aizu.ac.jp>
2021-08-10 04:25:35 +0900

    trgger.jsのdateオブジェクトを定義

--- trigger.js
+++ trigger.js
@@ -1,7 +1,15 @@
+const axios = require("axios");
+const dbAPI = "https://sheetdb.io/api/v1/1zz766ujclw94";
+
 exports.triggerFunc = () => {
     axios.get(`${dbAPI}`)
         .then(r => r.data)
         .then(data => {
+            const todayDate = new Date(); // 今日の日付を取得
+            const thisYear = todayDate.getFullYear();
+            const thisMonth = todayDate.getMonth() + 1;
+            const today = todayDate.getDate();
+
             for(let i=0; i<data.length; i++){
                 const userId = data[i].userId;
                 const tasks = JSON.parse(data[i].tasks);
